50ee641f6b8c486eed48b715b9f27480345d8cab
davidferro <david-ferro@sbcglobal.net>
2019-02-09 18:33:23 -0500

    Jaws da dun ...... da dun ....... da dun da dun da dun

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 var mysql = require("mysql");
 var keys = require("../keys.js");
 
-var connection = mysql.createConnection(keys.MySQL);
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(keys.MySQL);
+}
 
 connection.connect(function(err) {
   if (err) {
